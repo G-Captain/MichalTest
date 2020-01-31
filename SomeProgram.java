677cba36783ee9fcb779589afd52276c500cdfd2
MJ <michaljalowik@gmail.com>
2020-01-31 17:15:27 +0100

    dodana prosta funkcjonalnosc

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,7 +1,12 @@
-public class SomeProgram { 
+public class SomeProgram {
     public static void main(String[] args) {
-      System.out.println("We are learning to use Git."); 
-	  System.out.println("We will be Git experts, soon.");
+        System.out.println("We are learning to use Git.");
+        System.out.println("We will be Git experts, soon.");
+
+        int cyfra1 = 5;
+        int cyfra2 = 6;
+
+        System.out.println(cyfra1 + cyfra2);
 
     }
 }
